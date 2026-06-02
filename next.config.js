657eac34b893b90930788a0d123ee8fdf8fdbb21
raofarooqmuzammil-dot <raofarooq.muzammil@gmail.com>
2026-06-02 14:24:38 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/integrations",
+        destination: "/automation",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 module.exports = nextConfig;
